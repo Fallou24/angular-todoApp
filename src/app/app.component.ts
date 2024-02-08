8e4b5a7decc69a68e6a2242692ddba0cec5a6de6
Fallou24 <falloundiaye0221@gmail.com>
2024-02-08 12:44:52 +0000

    add task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TaskService } from './services/task.service';
 import { Observable, map } from 'rxjs';
 import { Task } from './models/task.model';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,28 @@ import { Task } from './models/task.model';
 })
 export class AppComponent implements OnInit {
   task$!: Observable<any>;
-  constructor(private taskService: TaskService) {}
+  taskControl!: FormControl;
+  constructor(
+    private taskService: TaskService,
+    private formBuilder: FormBuilder
+  ) {}
   ngOnInit(): void {
-    this.task$ = this.taskService.getTasks();
+    this.task$ = this.taskService.getTasks()
+    this.taskControl = this.formBuilder.control('', Validators.required);
+  }
+  onSubmitForm(e: Event) {
+    e.preventDefault();
+    const taskTitle = this.taskControl.value;
+    const newTask: Task = {
+      id: Date.now().toString(),
+      title: taskTitle,
+      status: false,
+      createdAt: new Date(),
+    };
+    this.taskService.createTask(newTask).subscribe(
+      (res: any) => console.log('Réponse de la requéte', res),
+      (err: any) => console.log('Erreur ', err)
+    );
+    this.taskControl.reset();
   }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Task } from '../models/task.model';
 
 @Injectable({
   providedIn: 'root',
@@ -8,5 +9,8 @@ export class TaskService {
   constructor(private http: HttpClient) {}
   getTasks() {
     return this.http.get('http://localhost:3000/todos');
+  }
+  createTask(data: Task) {
+    return this.http.post('http://localhost:3000/todos', data);
   }
 }
