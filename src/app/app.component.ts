01193bcc9f78e464a18a797da9d6a607a9a092f4
Fallou24 <falloundiaye0221@gmail.com>
2024-02-08 12:03:25 +0000

    Get todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from './services/task.service';
+import { Observable, map } from 'rxjs';
+import { Task } from './models/task.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-course';
+export class AppComponent implements OnInit {
+  task$!: Observable<any>;
+  constructor(private taskService: TaskService) {}
+  ngOnInit(): void {
+    this.task$ = this.taskService.getTasks();
+  }
 }
